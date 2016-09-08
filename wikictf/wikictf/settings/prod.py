66d893898df1cfde859958b49ce0ee512a8cb3c2
Anthony Li <antli.inbox@gmail.com>
2016-09-08 11:46:58 -0400

    Add database info

--- wikictf/wikictf/settings/prod.py
+++ wikictf/wikictf/settings/prod.py
@@ -1,5 +1,15 @@
 from __future__ import absolute_import # optional, but I like it
 from .common import *
-
+from . import *
 # Production overrides
 DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wikictf',
+        'USER': PRODUCTION_DATABASE_USER,
+        'PASSWORD': PRODUCTION_DATABASE_PASS,
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
