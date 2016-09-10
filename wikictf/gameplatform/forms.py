d2795376a1f2af6ec9d2bac01275bc80b7cd1944
Anthony Li <antli.inbox@gmail.com>
2016-09-10 16:45:50 -0400

    Make signup work

--- wikictf/gameplatform/forms.py
+++ wikictf/gameplatform/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from crispy_forms.helper import FormHelper
+from django.contrib.auth.models import User
 from crispy_forms.layout import Submit
+from django.core.exceptions import ValidationError
 class SignupForm(forms.Form):
     username = forms.CharField(
         label = "Username",
@@ -14,12 +16,24 @@ class SignupForm(forms.Form):
         required=True
 
     )
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput,
+                               label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput,
+                                label="Password (Again)")
 
     def __init__(self, *args, **kwargs):
         super(SignupForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_id = 'signupForm'
         self.helper.form_method = 'post'
-        self.helper.form_action = 'submitSignup'
+        self.helper.form_action = '.'
         self.helper.add_input(Submit('submit', 'Submit'))
+
+    def clean(self):
+        cleaned_data = super(SignupForm, self).clean()
+        if User.objects.filter(username=self.cleaned_data['username']).exists():
+            raise ValidationError('Username already exists.')
+        if cleaned_data.get('password') != cleaned_data.get('password2'):
+            raise ValidationError('Passwords do not match')
+
+        return cleaned_data
--- wikictf/gameplatform/views.py
+++ wikictf/gameplatform/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .forms import SignupForm
+from .models import Profile
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'gameplatform/index.html' )
 
+
 def signup(request):
-    return render(request, 'gameplatform/signup.html', {'sign_up': SignupForm()} )
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            # Create user
+            newuser = User.objects.create_user(form.cleaned_data['username'],
+                                               email=form.cleaned_data['email'],
+                                               password=form.cleaned_data['password'],
+                                               )
+            newuser.save()
+
+            # Create and save a profile object linked to this user
+            profile = Profile(user=newuser)
+            account_create_successful = "Account created successfully! You may now log in."
+            return render(request, 'gameplatform/message.html', {
+                'message': account_create_successful
+            })
+
+
+    else:
+        form = SignupForm()
+    return render(request, 'gameplatform/signup.html', {'sign_up': form} )
